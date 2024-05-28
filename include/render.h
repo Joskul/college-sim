9cc3c141e8893feae29af8d50d18c1342ff73d73
Joskul <53687187+Joskul@users.noreply.github.com>
2024-05-28 21:09:16 +0700

    Update render.h

--- include/render.h
+++ include/render.h
@@ -4,9 +4,6 @@
 #include "event.h"
 
 // Function prototypes
-void printProgress(double progress, double max);
-void delayedRenderLine(const char *str, const int time);
-void delayedRenderText(const char *str, const double time);
 action renderEvent(const event e, const gameInstance gd);
 int renderAction(const action act, const gameInstance gd);
 
